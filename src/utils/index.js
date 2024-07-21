46d8f43647d96f5277adc3b68f83b1038421ed9c
Noel Duggan <noelduggan21@gmail.com>
2024-07-21 07:46:01 +0000

    Updated files

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,8 +5,8 @@ export const heroImg = hero;
 import hmv from "/assets/videos/hero.mp4";
 import smallmv from "/assets/videos/smallHero.mp4";
 import highlightFirstmv from "/assets/videos/highlight-first.mp4";
-import highlightSectmv from "/assets/videos/hightlight-third.mp4";
-import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";
+import highlightSectmv from "/assets/videos/hightlight-third.mp4";  // Ensure this file exists and path is correct
+import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";  // Ensure this file exists and path is correct
 import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
 import exploremv from "/assets/videos/explore.mp4";
 import framemv from "/assets/videos/frame.mp4";
